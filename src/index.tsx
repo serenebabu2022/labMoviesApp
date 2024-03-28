466b288578fa90b0dd8ebba6a544884153e22c6f
serenebabu2022 <serenebabu22@gmail.com>
2024-03-28 19:04:49 +0000

    Movie Details Page rendering.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import HomePage from "./pages/homePage";
+import MovieDetailsPage from './pages/movieDetailsPage'
 import sample from './stories/sampleData'
+import { MovieImage } from "./types/interfaces";
 
-const movies = [sample, sample, sample, sample, sample, sample, sample];
+// const movies = [sample, sample, sample, sample, sample, sample, sample];
+const images: MovieImage[] = [
+    { file_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg" },
+    { file_path: "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg" },
+    { file_path: "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg" },
+    { file_path: "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg" },
+];
 
 const App = () => {
     return (
-        <HomePage movies={movies} />
+        <MovieDetailsPage movie={sample} images={images} />
     );
 };
 
